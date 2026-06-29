55b7270e70db723d3ded25e954cfde3d166edf5e
dnesbitt142 <99659447+dnesbitt142@users.noreply.github.com>
2026-06-29 21:01:58 +0100

    Initial spell checking

--- Installed/Library/Spell/dependencies.mk
+++ Installed/Library/Spell/dependencies.mk
@@ -1,3 +1,10 @@
+ET.obj \
+ET.eobj: ET/etManager.asm \
+                geos.def heap.def geode.def resource.def ec.def lmem.def \
+                thread.def sem.def chunkarr.def initfile.def library.def \
+                file.def object.def localize.def sllang.def \
+                etConstant.def etVariable.def spellConstant.def \
+                Internal/prodFeatures.def thes.asm thesC.asm
 UI.obj \
 UI.eobj: UI/uiManager.asm \
                 geos.def resource.def geode.def library.def ec.def \
@@ -31,45 +38,6 @@ UI.eobj: UI/uiManager.asm \
                 hugearr.def dbase.def Internal/spelllib.def \
                 uiConstant.def uiManager.rdef uiSuggestList.asm \
                 uiSpell.asm uiEditUserDict.asm thesCtrl.asm
-IH.obj \
-IH.eobj: IH/ihManager.asm \
-                geos.def heap.def geode.def resource.def ec.def lmem.def \
-                thread.def sem.def localize.def sllang.def \
-                Internal/threadIn.def initfile.def library.def file.def \
-                object.def ihConstants.def ihVariables.def ui.def vm.def \
-                text.def fontID.def graphics.def font.def color.def \
-                char.def win.def input.def hwr.def Objects/processC.def \
-                Objects/metaC.def chunkarr.def geoworks.def gcnlist.def \
-                timedate.def Objects/Text/tCommon.def stylesh.def \
-                iacp.def Objects/uiInputC.def Objects/visC.def \
-                Objects/vCompC.def Objects/vCntC.def Internal/vUtils.def \
-                Objects/genC.def disk.def drive.def uDialog.def \
-                Objects/gInterC.def token.def Objects/clipbrd.def \
-                Objects/gSysC.def Objects/gProcC.def alb.def \
-                Objects/gFieldC.def Objects/gScreenC.def \
-                Objects/gFSelC.def Objects/gViewC.def Objects/gContC.def \
-                Objects/gCtrlC.def Objects/gDocC.def Objects/gDocCtrl.def \
-                Objects/gDocGrpC.def Objects/gEditCC.def \
-                Objects/gViewCC.def Objects/gToolCC.def \
-                Objects/gPageCC.def Objects/gPenICC.def \
-                Objects/gGlyphC.def Objects/gTrigC.def \
-                Objects/gBoolGC.def Objects/gItemGC.def \
-                Objects/gDListC.def Objects/gItemC.def Objects/gBoolC.def \
-                Objects/gDispC.def Objects/gDCtrlC.def Objects/gPrimC.def \
-                Objects/gAppC.def Objects/gTextC.def Objects/gGadgetC.def \
-                Objects/gValueC.def Objects/gToolGC.def \
-                Internal/gUtils.def Objects/helpCC.def Objects/eMenuC.def \
-                Objects/emomC.def Objects/emTrigC.def Internal/uProcC.def \
-                Objects/vTextC.def spool.def print.def \
-                Internal/prodFeatures.def hugearr.def dbase.def \
-                ihCalls.asm
-ET.obj \
-ET.eobj: ET/etManager.asm \
-                geos.def heap.def geode.def resource.def ec.def lmem.def \
-                thread.def sem.def chunkarr.def initfile.def library.def \
-                file.def object.def localize.def sllang.def \
-                etConstant.def etVariable.def spellConstant.def \
-                Internal/prodFeatures.def thes.asm thesC.asm
 ICS.obj \
 ICS.eobj: ICS/icsManager.asm \
                 geos.def geode.def library.def ec.def heap.def file.def \
@@ -104,6 +72,40 @@ ICS.eobj: ICS/icsManager.asm \
                 Internal/icbuff.def icsConstant.def icsVariable.def \
                 spell.asm spellC.asm spellStrings.asm geos_asmcalls.asm \
                 geos_opts.asm icsThread.asm
+IH.obj \
+IH.eobj: IH/ihManager.asm \
+                geos.def heap.def geode.def resource.def ec.def lmem.def \
+                thread.def sem.def localize.def sllang.def \
+                Internal/threadIn.def initfile.def library.def file.def \
+                object.def ihConstants.def ihVariables.def ui.def vm.def \
+                text.def fontID.def graphics.def font.def color.def \
+                char.def win.def input.def hwr.def Objects/processC.def \
+                Objects/metaC.def chunkarr.def geoworks.def gcnlist.def \
+                timedate.def Objects/Text/tCommon.def stylesh.def \
+                iacp.def Objects/uiInputC.def Objects/visC.def \
+                Objects/vCompC.def Objects/vCntC.def Internal/vUtils.def \
+                Objects/genC.def disk.def drive.def uDialog.def \
+                Objects/gInterC.def token.def Objects/clipbrd.def \
+                Objects/gSysC.def Objects/gProcC.def alb.def \
+                Objects/gFieldC.def Objects/gScreenC.def \
+                Objects/gFSelC.def Objects/gViewC.def Objects/gContC.def \
+                Objects/gCtrlC.def Objects/gDocC.def Objects/gDocCtrl.def \
+                Objects/gDocGrpC.def Objects/gEditCC.def \
+                Objects/gViewCC.def Objects/gToolCC.def \
+                Objects/gPageCC.def Objects/gPenICC.def \
+                Objects/gGlyphC.def Objects/gTrigC.def \
+                Objects/gBoolGC.def Objects/gItemGC.def \
+                Objects/gDListC.def Objects/gItemC.def Objects/gBoolC.def \
+                Objects/gDispC.def Objects/gDCtrlC.def Objects/gPrimC.def \
+                Objects/gAppC.def Objects/gTextC.def Objects/gGadgetC.def \
+                Objects/gValueC.def Objects/gToolGC.def \
+                Internal/gUtils.def Objects/helpCC.def Objects/eMenuC.def \
+                Objects/emomC.def Objects/emTrigC.def Internal/uProcC.def \
+                Objects/vTextC.def spool.def print.def \
+                Internal/prodFeatures.def hugearr.def dbase.def \
+                ihCalls.asm
+osgspell.obj \
+osgspell.eobj: geos.h heap.h file.h geode.h
 uiManager.rdef: generic.uih product.uih spell.uih \
                 Internal/prodFeatures.uih spellFeatures.h \
                 UI/uiEditUserDict.ui UI/uiSpell.ui Art/mkrSpell.ui \
